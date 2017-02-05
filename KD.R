b99dd881b10971b3414e34a20d470b3361d2590c
raywu <raywu@yahoo-inc.com>
2017-02-05 19:56:41 +0800

    Read file and get data

--- KD.R
+++ KD.R
@@ -1,12 +1,12 @@
 # Include quantmod package
 library(quantmod)
 
-setwd("/Users/raywu/R")
+# Read file
+setwd("/Users/raywu/R") # work directory
 files <- list.files(path = "/Users/raywu/R", pattern ="*.list")
-
-
 file_count <- length(list.files( path = "/Users/raywu/R", pattern ="*.list"))
 
+# Get stock list
 for ( i in 1:file_count){
   assign ( files[i], read.table( files[i], header = FALSE, sep = "\n"))
 }
@@ -14,6 +14,7 @@ for ( i in 1:file_count){
 markets <- list( test.list, tw.list, sh.list, sz.list )
 markets_counts <- c( test = nrow(test.list), tw = nrow(tw.list), sh = nrow(sh.list), sz = nrow(sz.list))
 
+# Get stock data
 for ( x in 1:markets_counts[2] ){
   getSymbols(toString(tw.list[x,1]), src="yahoo", from="2017/01/01")
 }
